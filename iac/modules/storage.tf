08f5dbe7610615aecadde916a17068664ba1187b
Casey Colby <cecolby11@gmail.com>
2022-04-04 20:24:46 -0500

    update names

--- iac/modules/storage.tf
+++ iac/modules/storage.tf
@@ -1,6 +1,6 @@
 # bucket for files to be processed through the batch Dataflow ETL 
 resource "google_storage_bucket" "load" {
-  name                        = "raw_csv_files" # ensure globally unique
+  name                        = "${var.app_name}-raw" # ensure globally unique
   force_destroy               = false # delete files to delete bucket
   location                    = "US"
   uniform_bucket_level_access = true
@@ -11,7 +11,10 @@ resource "google_storage_bucket" "load" {
 # bucket for Dataflow JavaScript code containing your user-defined functions. 
 # Ex: gs://my-bucket/my-transforms/*.js
 resource "google_storage_bucket" "dataflow_functions" {
-    name          = "dataflow_functions"
+    name          = "${var.app_name}-dataflow-fxns"
     location      = "US"
     force_destroy = true
+    uniform_bucket_level_access = true
+
+    labels = local.labels
 }
