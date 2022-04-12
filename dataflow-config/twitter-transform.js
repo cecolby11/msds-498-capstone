85254b599466406c638a14532590242bb480ad71
Casey Colby <cecolby11@gmail.com>
2022-04-11 19:20:16 -0500

    update dataflow configs

--- dataflow-config/twitter-transform.js
+++ dataflow-config/twitter-transform.js
@@ -1,25 +1,36 @@
 function transform(line) {
     var values = line.split(',');
+    // validate line isn't missing data 
+    if(values.length < 7) {
+        return undefined; // tell dataflow to skip record
+    }
+
+    var cleaned = values.map(function(value) {
+        if (value === '') {
+            return null;
+        } else {
+            return value;
+        }
+    });
 
     var obj = new Object();
-    obj.tweet_id = values[0];
-    obj.author_id = values[1];
-    obj.inbound = values[2];
-    obj.created_at = values[3];
-    obj.text = values[4];
-    obj.response_tweet_id = values[5];
-    obj.in_response_to_tweet_id = values[6];
+    obj.tweet_id = cleaned[0];
+    obj.author_id = cleaned[1];
+    obj.inbound = cleaned[2];
+    obj.created_at = cleaned[3];
+    obj.text = cleaned[4];
+    obj.response_tweet_id = cleaned[5];
+    obj.in_response_to_tweet_id = cleaned[6];
 
     try {
         if (obj.created_at) {  
             timestamp = new Date(Date.parse(obj.created_at)).toISOString();
             obj.created_at = timestamp;
         }
+
+        var jsonString = JSON.stringify(obj);
+        return jsonString;
     } catch (err) {
-        obj.created_at = null;
+        // for now
     }
-
-    var jsonString = JSON.stringify(obj);
-
-    return jsonString;
 }
