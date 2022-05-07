51d0e30d012028eada626848b5c2ab9709282cd1
Casey Colby <cecolby11@gmail.com>
2022-05-07 10:48:24 -0500

    add a python script to get predictions

---
+++ src-python-client/README.md
@@ -0,0 +1,22 @@
+https://cloud.google.com/ai-platform/prediction/docs/online-predict
+
+
+```bash
+python -m venv venv
+source venv/bin/activate
+pip3 install google-api-python-client
+...
+deactivate
+```
+I had some issues of the python on my mac vs my venv version, because I did pip install of the google-api-python-client and then got `'googleapiclient' is not defined` type errors when running my script. so check which version of python is where:
+```bash
+which python
+# python: aliased to /usr/local/bin/python3
+which python3
+# /Users/caseycolby/git/msds/msds-498-capstone/msds-498-capstone/src-python-client/venv/bin/python3
+which pip
+# pip: aliased to pip3
+which pip3
+# /Users/caseycolby/git/msds/msds-498-capstone/msds-498-capstone/src-python-client/venv/bin/pip3
+``` 
+so now I know to run my script with `python3 app.py`
---
+++ src-python-client/app.py
@@ -0,0 +1,47 @@
+# Create the AI Platform service object.
+# To authenticate set the environment variable
+# GOOGLE_APPLICATION_CREDENTIALS=<path_to_service_account_file>
+import googleapiclient.discovery
+
+service = googleapiclient.discovery.build('ml', 'v1')
+
+
+def predict_json(project, model, instances, version=None):
+    """Send json data to a deployed model for prediction.
+
+    Args:
+        project (str): project where the AI Platform Model is deployed.
+        model (str): model name.
+        instances ([Mapping[str: Any]]): Keys should be the names of Tensors
+            your deployed model expects as inputs. Values should be datatypes
+            convertible to Tensors, or (potentially nested) lists of datatypes
+            convertible to tensors.
+        version: str, version of the model to target.
+    Returns:
+        Mapping[str: any]: dictionary of prediction results defined by the
+            model.
+    """
+    name = 'projects/{}/models/{}'.format(project, model)
+
+    if version is not None:
+        name += '/versions/{}'.format(version)
+
+    response = service.projects().predict(
+        name=name,
+        body={'instances': instances}
+    ).execute()
+
+    print(response['predictions'])
+
+    if 'error' in response:
+        raise RuntimeError(response['error'])
+
+    return response['predictions']
+
+
+instances = [
+        {"sex": "female", "age": 26, "smoker": False, "bmi":  27.315, "children": 0, "region": "northeast"},
+        {"sex": "male", "age": 55, "smoker": True, "bmi":  39.315, "children": 2, "region": "northeast"}
+    ]
+
+predict_json('dev-346101', 'simple_insurance', instances, 'console_test')
